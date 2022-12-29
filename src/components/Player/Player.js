520852110fd70d9357afb295d94a4dd46e65bdf2
Filipp Goriainov <barrios1991phil@gmail.com>
2022-12-29 18:34:22 +0400

    make pleload auto

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import classes from "./Player.module.scss";
 import pauseImg from "../../assets/images/pause.svg";
 import playImg from "../../assets/images/play.svg";
-function Player({ trackName, audioTag, isPlaying,togglePlay }) {
+function Player({ trackName, audioTag, isPlaying, togglePlay }) {
+
   const [currentImg, setCurrentImg] = useState(playImg);
 
   useEffect(() => {
@@ -16,14 +17,14 @@ function Player({ trackName, audioTag, isPlaying,togglePlay }) {
   console.log(trackName);
   return (
     <div className={classes["wrapper"]}>
-      <audio ref={audioTag} src={"audio/" + trackName + ".mp3"}></audio>
+      <audio preload="auto" ref={audioTag} src={"https://mp3.chillhop.com/serve.php/?mp3=9222"}></audio>
       {/* <button onClick={()=>audioTag.current.pause()} >pause</button> */}
       <div className={classes["container"]}>
         <div className={classes["timeline"]}>
           <div className={classes["buttons"]}>
             <button>prev</button>
             <button className={classes["play"]}>
-              <img onClick={()=>togglePlay(trackName)} src={currentImg} />
+              <img onClick={() => togglePlay(trackName)} src={currentImg} />
             </button>
             <button>next</button>
           </div>
