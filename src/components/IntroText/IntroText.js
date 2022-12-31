f5add0fd1e33b46e8ae9243d3e867a0e23f6dbfe
Filipp Goriainov <barrios1991phil@gmail.com>
2023-01-01 01:47:51 +0400

    check design

--- src/components/IntroText/IntroText.js
+++ src/components/IntroText/IntroText.js
@@ -2,11 +2,17 @@ import classes from './IntroText.module.scss';
 import React from 'react';
 
 function IntroText() {
-    return ( <p className={classes["text"]}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quisquam sunt, quod dignissimos est unde animi id accusamus voluptatibus sint repellat tenetur ratione veritatis sapiente odit enim distinctio! Nulla, magni?
-        Quasi magnam aliquid facilis dignissimos cumque aut consectetur laboriosam blanditiis dolorem dolorum minima, qui eaque itaque. Quaerat quasi perspiciatis beatae explicabo excepturi enim dolore maiores inventore aliquam, id tempora asperiores?
-        Repudiandae et quaerat minima veritatis, tempore error quasi reiciendis, libero aperiam commodi deleniti a dicta voluptatibus quisquam at nisi non asperiores corporis ut dolor obcaecati atque eos. Ipsa, aliquid in?
-    </p> );
+    return ( <section className={classes["text"]}>
+       <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum mollitia vero? A veritatis eaque, distinctio veniam atque tempore ea amet commodi assumenda impedit aliquid eveniet? Cupiditate porro aliquam ipsum?
+        Praesentium distinctio vel ut veniam earum quisquam at error alias perferendis ad placeat dolore, magnam accusantium animi, numquam natus. Facilis explicabo sequi neque ab vel optio esse corrupti? Id, reiciendis!
+       </p>
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur doloribus quod laboriosam repudiandae dolorem ex est consectetur corrupti debitis cum fuga quo iste quisquam, accusantium ratione voluptatum odit error dolorum.
+       Eaque sapiente natus laboriosam blanditiis obcaecati expedita quidem. Aut sunt quibusdam cupiditate iusto possimus voluptas. Illo sed aliquid commodi quibusdam tempora ab. Quae deserunt quam facere. Voluptatem maiores sequi officiis.
+       Voluptates, ullam? Commodi facere cum itaque sapiente voluptas fugiat, eaque nesciunt voluptatem provident ducimus nisi, unde dolores quod quisquam officiis repellat consectetur? Modi distinctio, hic quisquam unde beatae repellendus minima.</p>
+       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur quod impedit vel magnam maxime, modi error unde a at ducimus dolor consequatur beatae, harum porro, voluptates sunt. Repudiandae, magni quibusdam.</p>
+       
+    </section> );
 }
 
 export default IntroText;
